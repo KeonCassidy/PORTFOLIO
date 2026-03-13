6f7b9923fb9bde5f497558755cf9550c7ebf1438
KeonCassidy <keoncassidy26@gmail.com>
2026-03-13 12:24:59 +0800

    Add contact and resume sections to portfolio

--- Home.py
+++ Home.py
@@ -14,4 +14,31 @@ This portfolio showcases my:
 - Contact Information
 """)
 
+
 st.success("Use the sidebar to navigate through my portfolio pages.")
+
+st.title("Contact Me")
+
+st.write("You can contact me through the following:")
+
+st.write("📧 Email: keoncassidy26@email.com")
+st.write("📱 Phone: 09760654750")
+st.write("💻 GitHub: https://github.com/KeonCassidy")
+
+st.write("Thank you for visiting my portfolio!")
+
+st.title("My Resume")
+
+st.write("""
+### Education
+Bachelor of Science in Information Technology  
+(Rizal Technological University)
+
+### Certifications
+- Basic Python Programming
+- Networking Fundamentals
+
+### Experience
+- Academic Projects
+- Group Research in Systems Integration
+""")
