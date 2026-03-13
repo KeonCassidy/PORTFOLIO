505e86d6109be8e23001e564b7e07cc9bb2b6499
KeonCassidy <keoncassidy26@gmail.com>
2026-03-13 12:15:30 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Portfolio", page_icon="💻")
+
+st.title("Welcome to My Portfolio")
+
+st.write("Hello! I am a student passionate about technology and programming.")
+
+st.header("About This Website")
+st.write("""
+This portfolio showcases my:
+- Skills
+- Resume
+- Contact Information
+""")
+
+st.success("Use the sidebar to navigate through my portfolio pages.")
